75d493aff78fe41282fff4436cbf8c7829dd7d52
luangelsdorf <luanferreira2136@gmail.com>
2021-08-04 00:58:43 -0300

    Perform It functionality section

--- src/components/common/Functionalities/index.js
+++ src/components/common/Functionalities/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import styles from './Functionalities.module.scss';
+import Image from 'next/image';
 
 export default function Functionalities({ content }) {
-  console.log('func content')
-  console.log(content)
   return (
-    <section>
+    <section className={styles.funcSection}>
       <div className="container">
+        <div className="title-box">
+          <h2>Funcionalidades</h2>
+        </div>
         <div className="row">
-
+          {
+            content.map(func => {
+              return (
+                <div className="col-12 col-lg-4" key={func.title}>
+                  <div className={styles.cardFunc}>
+                    <div className={styles.icon}>
+                      <Image alt={func.title} src={require(`public/images/icones/${func.icon}`)} />
+                    </div>
+                    <div className={styles.content}>
+                      <h5>{func.title}</h5>
+                      <div className={styles.textBox} dangerouslySetInnerHTML={{__html: func.text}}/>
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          }
         </div>
       </div>
     </section>
