b985700ed7fa4e5c82611ba55226062efc04e674
luangelsdorf <luanferreira2136@gmail.com>
2021-08-08 18:41:20 -0300

    New Clients section

--- src/components/home/Clients/index.js
+++ src/components/home/Clients/index.js
@@ -16,19 +16,8 @@ export default function Clients({ content }) {
 
   return (
     <section className={styles.clientSection}>
-
-      <div className={`decoration ${styles.decor}`}><img src="/images/triangulos-clientes-esqu@2x.png" alt="Detalhe" /></div>
-      <div className={`decoration ${styles.decor}`}><img src="/images/triangulos-clientes-dire@2x.png" alt="Detalhe" /></div>
-      <div className={`decoration ${styles.decor}`}><img src="/images/triangulos-clientes-bottomm@2x.png" alt="Detalhe" /></div>
-
       <div className="container">
         <div className="row">
-          <div className="col-12">
-            <div className="clients-images-box d-flex justify-content-center">
-              <img className={`${styles.smallImg} img-fluid`} src="/images/clientes-box-small.jpg" alt="Investtools - Clientes" />
-              <img className={`${styles.bigImg} img-fluid`} src="/images/clientes-box-big.jpg" alt="Investtools - Clientes" />
-            </div>
-          </div>
           <div className="col-12">
             <TitleBox>Clientes</TitleBox>
             <div className="flickity">
@@ -37,7 +26,7 @@ export default function Clients({ content }) {
                   content.map(client => {
                     return (
                       <div className={`col-6 col-md-4 col-lg-3 ${styles.carouselItem}`} key={content.indexOf(client)}>
-                        <Image src={require(`../../../../public/images/clientes/${client.logo}`).default} alt={client.name} />
+                        <Image src={require(`public/images/clientes/${client.logo}`).default} alt={client.name} />
                       </div>
                     )
                   })
