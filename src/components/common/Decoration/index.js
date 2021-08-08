315e2fed803f553d9815886b2e2008dd0d73bd01
luangelsdorf <luanferreira2136@gmail.com>
2021-08-08 11:06:04 -0300

    Added Decoration component

---
+++ src/components/common/Decoration/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from './Decoration.module.scss';
+
+export default function Decoration(props) {
+
+  const decorName = `detail-${props.size}-${props.style}.svg`
+
+  return (
+    <div className={`${styles.decoration} ${props.className}`}>
+      <Image src={require(`public/images/new/${decorName}`).default} />
+    </div>
+  )
+}
