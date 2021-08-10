9301ae0009b6dd68ada00af2acb056792189b316
luangelsdorf <luanferreira2136@gmail.com>
2021-08-10 10:44:28 -0300

    Deployment fix

--- src/components/novos-negocios/NegociosHistory/index.js
+++ src/components/novos-negocios/NegociosHistory/index.js
@@ -12,7 +12,7 @@ export default function NegociosHistory() {
             </div>
           </div>
           <div className="col-12 offset-lg-1 col-lg-5 d-flex justify-content-center">
-            <div className="d-flex flex-column justify-content-center" className={styles.historyText}>
+            <div className={`${styles.historyText} d-flex flex-column justify-content-center`}>
               <p className="body-small">A Investtools surge precisamente como uma unidade de negócios da Ideais, empresa de tecnologia para diversos mercados. A trajetória de sucesso do grupo o leva a ser comprado como aquisição estratégica pela varejista B2W, e a Investtools passa por um spin-off, podendo se dedicar exclusivamente a fornecer soluções ao ecossistema financeiro.</p>
               <strong className="body-small">Situada então no Instituto Gênesis da PUC-Rio, berço de fintechs notórias, nossa história já nasce então de um case de sucesso.</strong>
             </div>
