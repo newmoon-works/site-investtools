8831c3f4727c9548f4a1151e9d1316c12bd1e8aa
luangelsdorf <luanferreira2136@gmail.com>
2021-08-10 19:02:06 -0300

    Visual adjustments

--- src/components/novos-negocios/CallToAction/index.js
+++ src/components/novos-negocios/CallToAction/index.js
@@ -14,7 +14,7 @@ export default function CallToAction() {
       <div className={styles.content}>
         <h4>Apresente sua Ideia para Nossa</h4>
         <h3>Equipe de Novos Negócios</h3>
-        <p>Investimos e desenvolvemos empresas que reforçam nosso ecossistema com acompanhamentos estratégicos e operacionais de forma tailor made. Se você é empreendedor ou tem uma ideia com solução de alto impacto e com potencial de escala, conte pra gente, vamos agendar uma conversa.</p>
+        <p className="body-small">Investimos e desenvolvemos empresas que reforçam nosso ecossistema com acompanhamentos estratégicos e operacionais de forma tailor made. Se você é empreendedor ou tem uma ideia com solução de alto impacto e com potencial de escala, conte pra gente, vamos agendar uma conversa.</p>
         <a
           href="http://investtools.rds.land/cta-site-novos-negocios"
           target="_blank"
