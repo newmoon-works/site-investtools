23d58ba7fda585acfe67d84f7ac7bdf683f854c2
luangelsdorf <luanferreira2136@gmail.com>
2021-08-11 23:55:25 -0300

    Added Call to Action on Products

--- src/components/common/AboutProduct/index.js
+++ src/components/common/AboutProduct/index.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import TitleBox from '../TitleBox';
 import Decoration from '../Decoration';
 
-export default function AboutProduct({ content: {about, features, title, href, logo, monitor} }) {
+export default function AboutProduct({ content: {about, features, title, href, ctaImage, logo, monitor} }) {
   return (
     <section className={styles.productSection}>
       <Decoration size="lg" style="normal" className={styles.decorTop} />
@@ -21,21 +21,19 @@ export default function AboutProduct({ content: {about, features, title, href, l
             </div>
           </div>
           <div className={styles.aboutProductBox}>
-            <div className="row">
+            <div className="row gx-lg-0">
               <div className="col-12 col-lg-5">
                 <div className={styles.aboutBox}>
-                  <Decoration size="xs" style="normal" className={styles.decorSmall} />
                   <TitleBox>{ title }</TitleBox>
                   <div className={styles.textBox} dangerouslySetInnerHTML={{__html: about}}/>
-                  <a href={href} className="ivt-btn-alt" target="_blank" rel="noopener noreferrer">
-                    <img src="/images/new/calendar.svg" alt="Calendar" />
-                    <span>Agende uma Demonstação</span>
+                  <a href={href} className={styles.cta} target="_blank" rel="noopener noreferrer">
+                    <img className="img-fluid d-block mx-auto" src={`/images/new/${ctaImage}`} alt="Calendar" />
                   </a>
                 </div>
               </div>
               <div className="col-12 col-lg-7">
                 <div className={styles.featureBox}>
-                  <Decoration size="lg" style="white2" className={styles.decorBig} />
+                  <Decoration size="xs" style="white" className={styles.decorBig} />
                   <div className={styles.feature}>
                     {
                       features.map(feat => {
