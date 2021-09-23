6b06eb537377da10100b9bd4bb6c9d88a2927452
luangelsdorf <luanferreira2136@gmail.com>
2021-09-22 21:47:21 -0300

    Social media share links

--- src/components/blog/PostBody/index.js
+++ src/components/blog/PostBody/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ShareButton from '../ShareButton';
 import styles from './PostBody.module.scss';
 
-export default function PostBody({content}) {
+export default function PostBody({ content }) {
   return (
     <div className={styles.body}>
-      <div dangerouslySetInnerHTML={{__html: content}} />
+      <div dangerouslySetInnerHTML={{ __html: content }} />
       <div className={styles.share}>
         <div className="row align-items-center">
           <div className="col-12 col-lg-6">
@@ -12,10 +13,10 @@ export default function PostBody({content}) {
           </div>
           <div className="col-12 col-lg-6">
             <div className={styles.socials}>
-              <div>F</div>
-              <div>L</div>
-              <div>T</div>
-              <div>E</div>
+              <ShareButton type="facebook" />
+              <ShareButton type="linkedin" />
+              <ShareButton type="twitter" />
+              <ShareButton type="email" />
             </div>
           </div>
         </div>
