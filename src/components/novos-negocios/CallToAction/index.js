2e1fe787a1045cdf3e4577e4ca2d3b3c31b7e71d
luangelsdorf <luanferreira2136@gmail.com>
2022-09-05 18:00:01 -0300

    Added Modal Call to Action

--- src/components/novos-negocios/CallToAction/index.js
+++ src/components/novos-negocios/CallToAction/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Decoration from 'src/components/common/Decoration';
+import Modal from 'src/components/common/Modal';
 import styles from './CallToAction.module.scss';
 
-export default function CallToAction({content}) {
+export default function CallToAction({ content }) {
   return (
     <section className={styles.section}>
 
@@ -15,14 +16,26 @@ export default function CallToAction({content}) {
         <h3>{content.title}</h3>
         <h4>{content.subtitle}</h4>
         <p>{content.text}</p>
-        <a
+        {/* <a
           href={content.cta.link}
           target="_blank"
           rel="noopener noreferrer"
           className="ivt-btn-alt">
           {content.cta.text}
-        </a>
+        </a> */}
+
+        <button
+          href="#modal-novos-negocios"
+          data-bs-toggle="modal"
+          className="ivt-btn-alt"
+        >
+          {content.cta.text}
+        </button>
       </div>
+
+      <Modal id="modal-novos-negocios">
+        Modal Novos Negócios
+      </Modal>
     </section>
   )
 }
