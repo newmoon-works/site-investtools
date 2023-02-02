6f097ec656558ad5eb65245a3964029b94de08f4
luangelsdorf <luanferreira2136@gmail.com>
2023-02-01 21:54:07 -0300

    Added layout params

--- src/lib/getLayoutContent.js
+++ src/lib/getLayoutContent.js
@@ -1,8 +1,8 @@
-export async function getLayoutContent(params) {
-  const resHeader = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/cabecalho`);
+export async function getLayoutContent(params = '') {
+  const resHeader = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/cabecalho${params}`);
   const header = await resHeader.json();
 
-  const resFooter = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/site`);
+  const resFooter = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/site${params}`);
   const footer = await resFooter.json();
 
   return { header, footer }
