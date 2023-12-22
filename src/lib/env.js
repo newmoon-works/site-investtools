4cfea1d9ea8fbfade52e02410d27f47e2a02518c
luangelsdorf <luanferreira2136@gmail.com>
2023-12-22 16:11:22 -0300

    Added Cookie Policy page

--- src/lib/env.js
+++ src/lib/env.js
@@ -1,1 +1,2 @@
 export const env = process.env.NEXT_PUBLIC_ENV;
+export const apiURL = process.env.NEXT_PUBLIC_API_URL;
---
+++ src/pages/politica-de-cookies.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Head from "next/head";
+import { getLayoutContent } from 'src/lib/getLayoutContent';
+import getLocaleParam from 'src/lib/getLocaleParam';
+import { apiURL } from 'src/lib/env';
+
+export default function CookiePolicy({ pageContent }) {
+  let content = pageContent.text.replaceAll('/uploads/', `${apiURL}/uploads/`);
+
+  return (
+    <>
+      <Head>
+        <title>Política de Cookies</title>
+      </Head>
+
+      <div className="container" style={{ marginTop: '32px' }}>
+
+        <style jsx-global>{`
+          img {
+            max-width: 75%;
+          }
+
+          @media screen and (max-width: 576px) {
+            img {
+              max-width: 100%;
+            }
+          }
+        `}</style>
+
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
+
+    </>
+  )
+}
+
+export async function getStaticProps({ locale }) {
+  let localeParameter = getLocaleParam(locale);
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/politica-de-cookies${localeParameter}`);
+  const pageContent = await res.json();
+
+  const contactRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/dados-de-contato${localeParameter}`);
+  const contact = await contactRes.json();
+
+  const layout = await getLayoutContent(localeParameter);
+
+  return {
+    props: { pageContent, layout, contact },
+    revalidate: 10,
+  }
+}
